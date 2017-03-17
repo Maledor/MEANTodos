a25e70bc36619bd9187e92d02bd8784c36e8f0ff
Maledor <josh.r.cameron@gmail.com>
2017-03-16 20:12:08 -0400

    Removed and cleaned up the controller and the index.html. We created the TodoService.

--- public/js/controllers/todo.controller.js
+++ public/js/controllers/todo.controller.js
@@ -2,15 +2,10 @@
   angular.module('MEANTodos') //Getter syntax
     .controller('TodoController', TodoController);
 
-    TodoController.$inject = ['$scope'];
+    TodoController.$inject = ['$scope', 'TodoService'];
 
-    function TodoController($scope){
-        $scope.message = 'I work!';
-        $scope.greeting = 'Hello';
-        $scope.print = print;
+    function TodoController($scope, TodoService){
 
-        function print(something){
-          console.log(something);
-        }
+      
       }
-    })()
+    })();
